911da0e31d97896379d455267b25792f1cc218e5
Taylor Fraley <tofraley@gmail.com>
2017-12-02 16:27:25 -0500

    Remove runAll.sh

--- runAll.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-dotnet run --project ./rabbitmq-example-api
-dotnet run --project ./AccountsAuditConsumer
-dotnet run --project ./DirectPaymentCardConsumer
-dotnet run --project ./PaymentCardConsumer
-dotnet run --project ./PurchaseOrderConsumer
